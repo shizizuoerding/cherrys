7973e3aa651c4d201efe616019d11f54dfc89823
ZYS <ZYS@DESKTOP-3EPJTET>
2017-12-11 18:02:00 +0800

    add

--- app.js
+++ app.js
@@ -133,6 +133,18 @@ app.get('/voice/:signature', function(req, res) {
   });
 });
 
+app.get('/voices/:signature', function(req, res) {
+    var signature = req.params.signature;
+    var query = new AV.Query("Voice");
+    query.equalTo('signature', signature);
+    query.first().then(function(results) {
+        console.log(results);
+        // 处理返回的结果数据
+        res.render('voice', {content:results.attributes.content,vcn:results.attributes.vcn});
+    }, function(error) {
+        console.log('Error: ' + error.code + ' ' + error.message);
+    });
+});
 app.post('/tts', function(req, res) {
   var content = req.body.content;
   var vcn = req.body.vcn;
@@ -161,6 +173,35 @@ app.post('/tts', function(req, res) {
     console.log('Failed to create new object, with error message: ' + err.message);
     res.send("102");
   });
+});
+app.post('/ttss', function(req, res) {
+    var content = req.body.content;
+    var vcn = req.body.vcn;
+    // var spd = req.body.spd;
+    // var vol = req.body.vol;
+    var timestamp = new Date().getTime();
+    // var signature = faultylabs.MD5(timestamp + '&' + content + '&' + content);
+    // md5.update(timestamp + '&' + vcn);
+    // var signature = md5.digest('hex');
+    var signature =timestamp + vcn;
+    console.log(signature);
+
+    var voice = AV.Object.new('Voice');
+    voice.set('content', content);
+    voice.set('vcn', vcn);
+    // voice.set('spd', spd);
+    // voice.set('vol', vol);
+    voice.set('signature', signature);
+    voice.save().then(function(post) {
+        // 成功保存之后，执行其他逻辑.
+        // console.log('New object created with objectId: ' + post.id);
+        res.send("http://cherrys.leanapp.cn/voices/"+signature);
+    }, function(err) {
+        // 失败之后执行其他逻辑
+        // error 是 AV.Error 的实例，包含有错误码和描述信息.
+        console.log('Failed to create new object, with error message: ' + err.message);
+        res.send("102");
+    });
 });
 
 
